e32d8077fefad7388e465ba7186adc0c45ae8cdd
NetAcad DEVASC <changeme@example.com>
2024-01-23 15:54:55 +0000

    Task 1 -- Github Skills Test

--- ScriptsTask1.sh
+++ ScriptsTask1.sh
@@ -1,21 +1,23 @@
-#Command for initializing a Git Repository:
+# Command for initializing a Git Repository:
 git init
 
 # Command for connecting Local Repository to Github Repository:
 git remote add origin https://github.com/ayaafellad/Devasc_Skills_AyaAfellad.git
 
-#Commands for adding and committing changes:
+# Commands for adding and committing changes:
 git add .
 git commit -m "Initial commit"
 
-#Command for pushing changes to GitHub:
+# Command for pushing changes to GitHub:
 git push -u origin master
 
-#commands to add, commit, and push "Task 1 -- Github Skills Test"
+# Commands to add, commit, and push "Task 1 -- Github Skills Test"
 git add .
-git commit -m "Task X: "Task 1 -- Github Skills Test"
+git commit -m "Task X: Task 1 -- Github Skills Test"
 git push
 
-#command to add a tag to a specific commit:
-tag -a task1 -m "task1_Github_Skills_Test"
+# Command to add a tag to a specific commit:
+git tag -a task1 -m "task1_Github_Skills_Test"
+
+# Command to push the tag to GitHub:
 git push origin task1
